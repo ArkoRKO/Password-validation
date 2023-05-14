70ab888cdd404cd3114da96f5a4f2d29332a3ae0
Arkadiusz Grzegorczyk <a.grzegorczyk0@gmail.com>
2023-05-14 16:24:32 +0200

    logic upgrade

--- js/script.js
+++ js/script.js
@@ -1,49 +1,58 @@
-const clear = document.querySelector('.clear')
-const username = document.querySelector('#username')
-const pass = document.querySelector('#password')
-const pass2 = document.querySelector('#password2')
-const email = document.querySelector('#email')
-const sendBtn = document.querySelector('.send')
-const clearBtn = document.querySelector('.clear')
-const popup = document.querySelector('.popup')
-const loginForm = document.getElementById('loginForm')
+const clear = document.querySelector('.clear');
+const username = document.querySelector('#username');
+const pass = document.querySelector('#password');
+const pass2 = document.querySelector('#password2');
+const email = document.querySelector('#email');
+const sendBtn = document.querySelector('.send');
+const clearBtn = document.querySelector('.clear');
+const popup = document.querySelector('.popup');
+const closePopup = document.querySelector('.close');
 
 const showError = (input, msg) => {
-	const formBox = input.parentElement
-	const errorMsg = formBox.querySelector('.error-text')
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector('.error-text');
 
-	formBox.classList.add('error')
-	errorMsg.textContent = msg
-}
+	formBox.classList.add('error');
+	errorMsg.textContent = msg;
+};
 
 const checkForm = (input) => {
 	input.forEach((el) => {
 		if (el.value === '') {
-			showError(el, el.placeholder)
+			showError(el, el.placeholder);
 		} else {
-			console.log('ok')
+			showPopup();
 		}
-	})
-}
+	});
+};
+
+const showPopup = () => {
+	if (pass.value !== '' && pass2.value !== '') {
+		popup.classList.add('show-popup');
+	}
+};
 
 // argument INPUT z funkcji "checkform" przechowuje tablicę z naszymi inputami
 // argument EL odnosi się do każdej zmiennej, którą umieściliśmy w tablicy
 
-// loginForm.addEventListener('submit', (e) => {
-// 	e.preventDefault();
+closePopup.addEventListener('click', (e) => {
+	e.preventDefault();
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = '';
+	});
 
-// 	checkForm([username, pass, pass2, email])
-// })
+	popup.classList.remove('show-popup');
+});
 
 sendBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 
-	checkForm([username, pass, pass2, email])
-})
+	checkForm([username, pass, pass2, email]);
+});
 
 clearBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-	([username, pass, pass2, email]).forEach((el) => {
-		el.value = ''
-	})
-})
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = '';
+	});
+});
